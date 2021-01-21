36c23c79d9d23f9b9f89dfd90e9d4eba1b861095
shivamshukla99 <71917126+shivamshukla99@users.noreply.github.com>
2021-01-21 15:12:32 +0530

    Add files via upload
    This is the day 1 assignment of DSA

---
+++ day1 assigment/assignment1.py
@@ -0,0 +1,10 @@
+# Question no 1
+#********************* Answer 1 *******************
+# O(N + M) time, O(1) space
+print("answer 1: O(N + M) time, O(1) space")
+
+
+# Question no 2
+#********************* Answer 2 *******************
+# b)X will be a better choice for all inputs except possibly small inputs
+print("answer 2:  b)X will be a better choice for all inputs except possibly small inputs")
